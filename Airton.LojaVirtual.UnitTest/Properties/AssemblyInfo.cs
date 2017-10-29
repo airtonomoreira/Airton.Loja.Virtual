00696449f289eea9caa4d2d148197207a887729a
airtonomoreira <32795937+airtonomoreira@users.noreply.github.com>
2017-10-29 12:45:25 -0200

    teste Git

---
+++ Airton.LojaVirtual.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Airton.LojaVirtual.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Airton.LojaVirtual.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b26c1e49-0dd0-4fd9-94f7-9e93b984126b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Airton.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Airton.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestAirton
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
